e4e94d516fbffaf89ae6bb25d1766c44cfcb5c87
yingweng <yingweng1120112@gmail.com>
2024-03-14 13:02:19 +0800

    更新資料庫連接資訊

--- parts/pdo-connect.php
+++ parts/pdo-connect.php
@@ -2,9 +2,9 @@
 <?php
 
 $db_host = "localhost";
-$db_user = 'root';
-$db_pass = '';
-$db_name = 'poj57';
+$db_user = 'admin';
+$db_pass = 'admin';
+$db_name = 'take_pet_away';
 
 $dsn = "mysql:host={$db_host};dbname={$db_name};charset=utf8mb4";
 
