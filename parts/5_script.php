6fa20c4f8cd13053556fe0be97bb97eb2f31794f
yingweng <yingweng1120112@gmail.com>
2024-03-14 12:09:07 +0800

    origin

---
+++ parts/5_script.php
@@ -0,0 +1,8 @@
+<script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+      crossorigin="anonymous"
+></script>
+<script
+  src="https://use.fontawesome.com/releases/v6.3.0/js/all.js"
+  crossorigin="anonymous"
+></script>
---
+++ parts/pdo-connect.php
@@ -0,0 +1,17 @@
+<!-- 先連上課範例資料庫 ( 參考用 暫放) -->
+<?php
+
+$db_host = "localhost";
+$db_user = 'root';
+$db_pass = '';
+$db_name = 'poj57';
+
+$dsn = "mysql:host={$db_host};dbname={$db_name};charset=utf8mb4";
+
+$pdo_options = [
+  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+
+$pdo = new PDO($dsn, $db_user, $db_pass, $pdo_options);
