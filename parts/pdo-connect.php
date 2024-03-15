1a65e760466f56d5681a625181d5a992dac1fa2b
xuanqi35 <xuanqi357@gmail.com>
2024-03-15 20:10:29 +0800

    資料庫檔名

--- parts/pdo-connect.php
+++ parts/pdo-connect.php
@@ -2,9 +2,9 @@
 <?php
 
 $db_host = "localhost";
-$db_user = 'admin';
-$db_pass = 'admin';
-$db_name = 'take_pet_away';
+$db_user = "root";
+$db_pass = "";
+$db_name = "proj01";
 
 $dsn = "mysql:host={$db_host};dbname={$db_name};charset=utf8mb4";
 
@@ -15,3 +15,20 @@ $pdo_options = [
 
 
 $pdo = new PDO($dsn, $db_user, $db_pass, $pdo_options);
+
+/*
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "proj01";
+
+$dsn = "mysql:host={$db_host};dbname={$db_name};charset=utf8mb4";
+
+$pdo_options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+
+$pdo = new PDO($dsn, $db_user, $db_pass, $pdo_options);
+*/
