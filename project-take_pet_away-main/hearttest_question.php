424ff856cf79b5d33fcf704d448a923dcc48377f
Shan <lightful2013@gmail.com>
2024-03-15 10:44:50 +0800

    test123

--- project-take_pet_away-main/hearttest_question.php
+++ project-take_pet_away-main/hearttest_question.php
@@ -7,7 +7,7 @@ require __DIR__ . '/parts/pdo-connect.php';
 $title = '通訊錄列表';
 $pageName = 'list';
 $pageName = 'list123';
-
+/*123*/
 //7-1.分頁功能
 $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
 
