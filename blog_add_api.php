df5d4fbaa829ef7cc7432434ab6bee15402cad38
paul <a0921675081@gmail.com>
2024-03-19 13:06:04 +0800

    修正名稱以及add新增pic欄位

---
+++ blog_add_api.php
@@ -0,0 +1,32 @@
+<?php
+
+require __DIR__ . '/parts/pdo-connect.php';
+
+$output = [
+  'success' => false,
+  'postData' => $_POST, # 除錯用
+  'error' => '',
+  'code' => 0, # 除錯或追踪程式碼
+];
+
+
+
+if (!empty($_POST['pet_id'])) {
+
+  $sql = "INSERT INTO `blog`(
+    `pet_id`, `content`, `time`
+    ) VALUES(
+    ?, ?,NOW()
+  )";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute([
+    $_POST['pet_id'],
+    $_POST['content'],
+  ]);
+
+  $output['success'] = boolval($stmt->rowCount());
+}
+
+header('Content-Type: application/json');
+
+echo json_encode($output, JSON_UNESCAPED_UNICODE);
