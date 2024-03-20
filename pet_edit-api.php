fcab7bf143cf61e0d354d8eec2489dbc7e36f1c2
xuanqi35 <xuanqi357@gmail.com>
2024-03-20 17:11:58 +0800

    新增 上傳圖片審核和繼承選項

--- pet_edit-api.php
+++ pet_edit-api.php
@@ -1,5 +1,6 @@
 <?php
 // require __DIR__."/parts/admin-required.php";
+$dir = __DIR__ . '/uploads/'; # 存放檔案的資料夾
 require __DIR__ . "/parts/pdo-connect.php";
 header("Content-Type: application/json");
 
@@ -9,7 +10,27 @@ $output = [
   "postData" => $_POST,   # 除錯用
   "error" => "",
   "code " => 0,   # 除錯或追蹤程式碼用
+  'file' => ''
 ];
+
+$exts = [   # 檔案類型的篩選
+  'image/jpeg' => '.jpg',
+  'image/png' =>  '.png',
+  'image/webp' => '.webp',
+];
+$output['file'] = $_POST['pic'];
+# 確保有上傳檔案，並且有 avatar 欄位，並且沒有錯誤
+if (!empty($_FILES) and !empty($_FILES['avatar']) and $_FILES['avatar']['error'] == 0) {
+  # 如果類型有對應到副檔名
+  $type = $_FILES['avatar']['type'];
+  if (!empty($exts[$type])) {
+    $ext = $exts[$type]; # 副檔名
+    $f = sha1($_FILES['avatar']['name'] . uniqid()); # 隨機的主檔名
+    if (move_uploaded_file($_FILES['avatar']['tmp_name'], $dir . $f . $ext)) {
+      $output['file'] = $f . $ext;
+    }
+  }
+}
 
 if (!empty($_POST["pet_id"]) and !empty($_POST["name"])) {
   $isPass = true;
@@ -33,9 +54,9 @@ if (!empty($_POST["pet_id"]) and !empty($_POST["name"])) {
               `age`=?,
               `type`=?,
               `sex`=?,
-              `adopted`=?
+              `adopted`=?,
+              `pet_pic1`=?
             WHERE `pet_id`=?";
-
     $stmt = $pdo->prepare($sql);
     $stmt->execute([
       $_POST['name'],
@@ -45,7 +66,8 @@ if (!empty($_POST["pet_id"]) and !empty($_POST["name"])) {
       $_POST['type'],
       $_POST['sex'],
       $_POST['adopted'],
-      $_POST['pet_id'],
+      $output['file'],
+      $_POST['pet_id']
     ]);
 
 
--- pet_edit.php
+++ pet_edit.php
@@ -43,24 +43,24 @@ if (empty($row)) {
           <!-- <h5 class="card-title my-3">新增寵物</h5> -->
           <form name="form1" onsubmit="sendData(event)">
             <div class="mb-3">
-              <input type="hidden" name="pet_id" value="<?= $row['pet_id'] ?>">
+              <input type="hidden" name="pet_id" value="<?= $row["pet_id"] ?>">
               <div class="mb-3">
                 <label for="name" class="form-label">編號</label>
-                <input type="text" class="form-control" id="pet_id" name="pet_id" value="<?= $row['pet_id'] ?>" disabled>
+                <input type="text" class="form-control" id="pet_id" name="pet_id" value="<?= $row["pet_id"] ?>" disabled>
               </div>
               <div class="mb-3">
                 <label for="name" class="form-label">名字</label>
-                <input type="text" class="form-control" id="name" name="name">
+                <input type="text" class="form-control" id="name" name="name" value="<?= $row["name"] ?>">
                 <div class="form-text"></div>
               </div>
               <div class="mb-3">
                 <label for="tag" class="form-label">hashtag</label>
-                <input type="text" class="form-control" id="tag" name="tag">
+                <input type="text" class="form-control" id="tag" name="tag" value="<?= $row["tag"] ?>">
                 <div class="form-text"></div>
               </div>
               <div class="mb-3">
                 <label for="age" class="form-label">年齡</label>
-                <input type="number" class="form-control" id="age" name="age">
+                <input type="number" class="form-control" id="age" name="age" value="<?= $row["age"] ?>">
                 <div class="form-text"></div>
               </div>
               <!-- <div class="mb-3">
@@ -70,17 +70,16 @@ if (empty($row)) {
               </div> -->
               <div class="mb-3">
                 <label for="personality_type" class="form-label me-3 lh-lg">性格類型</label>
-                <input type="radio" class="btn-check" name="personality_type" id="personality_type1" value="2" autocomplete="off" checked>
+                <input type="radio" class="btn-check" name="personality_type" id="personality_type1" value="敏感型" autocomplete="off" <?= ("敏感型" == $row["personality_type"]) ? "checked" : "" ?>>
                 <label class="btn btn-outline-secondary" for="personality_type1">敏感型</label>
-                <input type="radio" class="btn-check" name="personality_type" id="personality_type2" value="樂天型" autocomplete="off">
+                <input type="radio" class="btn-check" name="personality_type" id="personality_type2" value="樂天型" autocomplete="off" <?= ("樂天型" == $row["personality_type"]) ? "checked" : "" ?>>
                 <label class="btn btn-outline-secondary" for="personality_type2">樂天型</label>
-                <input type="radio" class="btn-check" name="personality_type" id="personality_type3" value="獨立型" autocomplete="off">
+                <input type="radio" class="btn-check" name="personality_type" id="personality_type3" value="獨立型" autocomplete="off" <?= ("獨立型" == $row["personality_type"]) ? "checked" : "" ?>>
                 <label class="btn btn-outline-secondary" for="personality_type3">獨立型</label>
-                <input type="radio" class="btn-check" name="personality_type" id="personality_type4" value="自信型" autocomplete="off">
+                <input type="radio" class="btn-check" name="personality_type" id="personality_type4" value="自信型" autocomplete="off" <?= ("自信型" == $row["personality_type"]) ? "checked" : "" ?>>
                 <label class="btn btn-outline-secondary" for="personality_type4">自信型</label>
-                <input type="radio" class="btn-check" name="personality_type" id="personality_type5" value="適應型" autocomplete="off">
+                <input type="radio" class="btn-check" name="personality_type" id="personality_type5" value="適應型" autocomplete="off" <?= ("適應型" == $row["personality_type"]) ? "checked" : "" ?>>
                 <label class="btn btn-outline-secondary" for="personality_type5">適應型</label>
-                <div class="form-text"></div>
               </div>
               <!-- <div class="mb-3">
                 <label for="type" class="form-label">種類</label>
@@ -94,27 +93,24 @@ if (empty($row)) {
               </div> -->
               <div class="mb-3">
                 <label for="type" class="form-label me-3 lh-lg">種類</label>
-                <input type="radio" class="btn-check" name="type" id="type-dog" value="2" autocomplete="off" checked>
+                <input type="radio" class="btn-check" name="type" id="type-dog" value="狗" autocomplete="off" <?= ("狗" == $row["type"]) ? "checked" : "" ?>>
                 <label class="btn btn-outline-secondary" for="type-dog">狗狗</label>
-                <input type="radio" class="btn-check" name="type" id="type-cat" value="貓" autocomplete="off">
+                <input type="radio" class="btn-check" name="type" id="type-cat" value="貓" autocomplete="off" <?= ("貓" == $row["type"]) ? "checked" : "" ?>>
                 <label class="btn btn-outline-secondary" for="type-cat">貓貓</label>
-                <div class="form-text"></div>
               </div>
               <div class="mb-3">
                 <label for="sex" class="form-label me-3 lh-lg">性別</label>
-                <input type="radio" class="btn-check" name="sex" id="sex-boy" value="2" autocomplete="off" checked>
+                <input type="radio" class="btn-check" name="sex" id="sex-boy" value="公" autocomplete="off" <?= ("公" == $row["sex"]) ? "checked" : "" ?>>
                 <label class="btn btn-outline-secondary" for="sex-boy">男生</label>
-                <input type="radio" class="btn-check" name="sex" id="sex-girl" value="母" autocomplete="off">
+                <input type="radio" class="btn-check" name="sex" id="sex-girl" value="母" autocomplete="off" <?= ("母" == $row["sex"]) ? "checked" : "" ?>>
                 <label class="btn btn-outline-secondary" for="sex-girl">女生</label>
-                <div class="form-text"></div>
               </div>
               <div class="mb-3">
                 <label for="adopted" class="form-label me-3 lh-lg">是否被領養</label>
-                <input type="radio" class="btn-check" name="adopted" id="adopted-True" value="2" autocomplete="off" checked>
+                <input type="radio" class="btn-check" name="adopted" id="adopted-True" value="是" autocomplete="off" <?= ("是" == $row["adopted"]) ? "checked" : "" ?>>
                 <label class="btn btn-outline-secondary" for="adopted-True">是</label>
-                <input type="radio" class="btn-check" name="adopted" id="adopted-False" value="否" autocomplete="off">
+                <input type="radio" class="btn-check" name="adopted" id="adopted-False" value="否" autocomplete="off" <?= ("否" == $row["adopted"]) ? "checked" : "" ?>>
                 <label class="btn btn-outline-secondary" for="adopted-False">否</label>
-                <div class="form-text"></div>
               </div>
               <!-- <label for="adopted" class="form-label me-3">是否被領養</label>
               <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
@@ -123,11 +119,20 @@ if (empty($row)) {
                 <input type="radio" class="btn-check" name="adopted" id="adoptedFalse" autocomplete="off">
                 <label class="btn btn-outline-secondary" for="adoptedFalse">否</label>
               </div> -->
-            </div>
-            <div class="input-group mb-3">
-              <label for="file" class="form-label me-3 lh-lg">上傳寵物照片</label>
-              <!-- <label class="input-group-text" for="inputGroupFile01"></label> -->
-              <input type="file" class="form-control" id="inputGroupFile01">
+              <div class="mb-3">
+                <input type="file" id="previewImage" name="avatar" accept="image/jpeg,image/png">
+                <img id="show_image" src="">
+                <img id="imgEdit" src="uploads/<?= $row["pet_pic1"] ?>" alt="">
+              </div>
+              <!-- <div class="input-group mb-3">
+                <input type="file" class="form-control" id="inputGroupFile02">
+                <label class="input-group-text" for="inputGroupFile02">Upload</label>
+              </div> -->
+              <!-- TODO: input要不要留 -->
+              <!-- FIXME: 上傳照片排版 -->
+              <div id="imgEdit" class="mb-3">
+                <input style="display: none;" name="pic" value=<?= $row["pet_pic1"] ?>>
+              </div>
             </div>
             <div class="d-flex justify-content-end mt-5">
               <button type="reset" class="btn btn-secondary w-25 mx-4">CANCEL</button>
@@ -139,7 +144,6 @@ if (empty($row)) {
     </div>
   </div>
 </div>
-
 <!-- Modal -->
 <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
@@ -199,7 +203,6 @@ if (empty($row)) {
     let isPass = true; // 有沒有通過檢查，預設為T
 
     // TODO: 檢查資料格式
-
     // 姓名 長度2以上
     if (nameField.value.length < 2) {
       isPass = false;
@@ -256,13 +259,40 @@ if (empty($row)) {
           failureInfo.innerHTML = `<i class="fa-regular fa-circle-xmark me-2"></i>"資料修改失敗"` + ex;
           failureModal.show();
         })
-
     }
-
   }
 
-  const successModal = new bootstrap.Modal('#successModal');
-  const failureModal = new bootstrap.Modal('#failureModal');
-  const failureInfo = document.querySelector('#failureModal .alert-danger');
+  // TODO: 照片上傳
+  // FIXME: 照片大小排版
+  var imageProc = function(input) {
+    if (input.files && input.files[0]) {
+      // 建立一個 FileReader 物件
+      var reader = new FileReader();
+      // 當檔案讀取完後，所要進行的動作
+      reader.onload = function(e) {
+        // 顯示圖片
+        $("#show_image")
+          .attr("src", e.target.result)
+          .css("height", "100px")
+          .css("width", "auto");
+        $("#imgEdit")
+          .css("height", "100px")
+          .css("width", "auto")
+          .css("display", "none");
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
+  };
+
+  $(document).ready(function() {
+    // 綁定事件
+    $("#previewImage").change(function() {
+      imageProc(this);
+    });
+  });
+
+  const successModal = new bootstrap.Modal("#successModal");
+  const failureModal = new bootstrap.Modal("#failureModal");
+  const failureInfo = document.querySelector("#failureModal .alert-danger");
 </script>
 <?php include __DIR__ . "/parts/6_foot.php" ?>
