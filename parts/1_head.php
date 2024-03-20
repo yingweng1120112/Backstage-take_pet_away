c5998023ec8b30385d6fdb6b2c060da215c8848b
paul <a0921675081@gmail.com>
2024-03-20 10:32:22 +0800

    1

--- parts/1_head.php
+++ parts/1_head.php
@@ -9,14 +9,6 @@
   <meta name="author" content="" />
   <title><?= empty($title) ? "無名網頁" : "$title" ?></title>
   <link href="css/styles.css" rel="stylesheet" />
-  <style>
-    .pic-show-short {
-      width: 350px;
-      overflow: hidden;
-      white-space: nowrap;
-      text-overflow: ellipsis
-    }
-  </style>
 </head>
 
 <body class="sb-nav-fixed">
--- test/blobtest.php
+++ test/blobtest.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/parts/pdo-connect.php';
+require __DIR__ . '/../parts/pdo-connect.php';
 
 $output = [
   'success' => false,
