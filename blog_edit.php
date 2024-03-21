8cb7c4778a2e2917dfb4df14fbbeb80197f65de6
paul <a0921675081@gmail.com>
2024-03-21 14:29:49 +0800

    修正細節

--- blog_edit.php
+++ blog_edit.php
@@ -31,9 +31,9 @@ if (empty($r)) {
           <h5 class="card-title">編輯</h5>
 
           <form name="form1" onsubmit="sendData(event)">
-            <input type="text" style="display: none;" class="form-control" name="blog_id" value="<?= $r['blog_id'] ?>">
+            <input type="hidden" class="form-control" name="blog_id" value="<?= $r['blog_id'] ?>">
             <div class="mb-3">
-              <input type="text" style="display: none;" class=" form-control" id="pet_id" name="pet_id" value="<?= $r['pet_id'] ?>">
+              <input type="hidden" class=" form-control" id="pet_id" name="pet_id" value="<?= $r['pet_id'] ?>">
               <div class="form-text"></div>
             </div>
             <div class="mb-3">
@@ -41,14 +41,15 @@ if (empty($r)) {
               <textarea class="form-control" name="content" id="content" cols="30" rows="3"><?= $r['content'] ?></textarea>
               <div class="form-text"></div>
             </div>
-            <input style="display: none;" type="text" name="pic" value="<?= $r['pic'] ?>">
             <div class="mb-3">
               <input type="file" id="previewImage" name="avatar" accept="image/jpeg,image/png" />
               <br />
               <img id="show_image" src="" />
             </div>
-            <div id="a">
-              <p>當前檔案</p> <img src="uploads/<?= $r['pic'] ?>" alt="">
+            <div id="now_pic">
+              <!-- 原始圖片 -->
+              <input type="hidden" name="pic" value="<?= $r['pic'] ?>">
+              <p>當前檔案</p> <img width="200px" src="uploads/<?= $r['pic'] ?>" alt="">
             </div>
             <button type="submit" class="btn btn-primary">修改</button>
           </form>
@@ -101,7 +102,6 @@ if (empty($r)) {
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script>
   const {
-    pet_id: pet_idField,
     content: contentField,
   } = document.form1;
 
@@ -109,8 +109,6 @@ if (empty($r)) {
 
   function sendData(e) {
 
-    pet_idField.style.border = "1px solid #CCC";
-    pet_idField.nextElementSibling.innerHTML = '';
     contentField.style.border = "1px solid #CCC";
     contentField.nextElementSibling.innerHTML = '';
 
@@ -124,6 +122,7 @@ if (empty($r)) {
       contentField.style.border = "2px solid red";
       contentField.nextElementSibling.innerHTML = '請輸入正確的內容';
     }
+
     if ($isPass) {
       const fd = new FormData(document.form1); // 看成沒有外觀的表單
       fetch('blog_edit_api.php', {
@@ -165,7 +164,7 @@ if (empty($r)) {
           .attr("src", e.target.result)
           .css("height", "100px")
           .css("width", "100px");
-        $("#a").css("display", "none");
+        $("#now_pic").css("display", "none");
       };
       reader.readAsDataURL(input.files[0]);
     }
