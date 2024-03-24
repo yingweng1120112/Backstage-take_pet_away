f5a1d6c29982127cc8f7fd56a8900d5fb8fb1756
yingweng <yingweng1120112@gmail.com>
2024-03-24 22:24:39 +0800

    更新dalete

--- user-delete.php
+++ user-delete.php
@@ -2,7 +2,7 @@
 require __DIR__ . '/parts/admin-required.php';
 require __DIR__ . '/parts/pdo-connect.php';
 
-$sid = isset($_GET['user_id']) ? intval($_GET['user_id']) : 0;
+$user_id = isset($_GET['user_id']) ? intval($_GET['user_id']) : 0;
 
 if (!empty($user_id)) {
   $sql = "DELETE FROM user WHERE user_id=$user_id";
